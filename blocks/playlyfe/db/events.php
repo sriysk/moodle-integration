5c18f31df4ce922212148839e071e44f442b6d6c
Peter John <peter@playlyfe.com>
2015-05-29 18:02:03 +0530

    feat add events and observers

---
+++ blocks/playlyfe/db/events.php
@@ -0,0 +1,16 @@
+<?php
+
+$observers = array(
+    array(
+        'eventname' => '\core\event\user_created',
+        'callback' => 'block_playlyfe_observer::create_player',
+    ),
+    array(
+       'eventname'   => '\core\event\user_loggedin',
+       'callback'    => 'block_playlyfe_observer::log_in',
+   ),
+   array(
+       'eventname'   => '\core\event\user_loggedout',
+       'callback'    => 'block_playlyfe_observer::log_out',
+   )
+);
